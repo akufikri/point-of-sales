eb70aa834360004963147e1b3da3fe6900d5c401
Fikri Nurhakim <108182945+akufikri@users.noreply.github.com>
2024-01-16 13:53:58 +0700

    🚀 { add: Fixing dynamic dropdown, and add notification after submition products  }

--- app/Livewire/Transaksi.php
+++ app/Livewire/Transaksi.php
@@ -2,7 +2,9 @@
 
 namespace App\Livewire;
 
+use App\Models\DetailPenjualan;
 use App\Models\Pelanggan;
+use App\Models\Penjualan;
 use App\Models\Produk;
 use Livewire\Component;
 
@@ -11,31 +13,31 @@ class Transaksi extends Component
     public $produk;
     public $selectedProduk = [];
     public $pelanggan;
-
-    protected $listeners = ['refreshComponent' => '$refresh'];
-
+    public $feeAdmin = 10;
+    public $qty = 1;
+    public $selectedPelangganId;
+    protected $rules = [
+        'selectedPelangganId' => 'required',
+        'selectedProduk.*.quantity' => 'required|integer|min:1',
+        'feeAdmin' => 'required|numeric|min:10',
+    ];
     public function mount()
     {
+        $this->selectedProduk = [];
         $this->produk = Produk::all();
         $this->pelanggan = Pelanggan::all();
+        $this->selectedPelangganId = null;
     }
 
     public function selectProduk($productId)
     {
-        // Cek apakah produk sudah ada dalam array selectedProduk
-        $existingIndex = null;
-        foreach ($this->selectedProduk as $index => $item) {
-            if ($item['product']->id == $productId) {
-                $existingIndex = $index;
-                break;
-            }
-        }
+        $existingIndex = collect($this->selectedProduk)->search(function ($item) use ($productId) {
+            return $item['product']->id == $productId;
+        });
 
-        if ($existingIndex !== null) {
-            // Produk sudah ada, mungkin bisa menampilkan pesan atau melakukan aksi lain
-            // Misalnya: $this->addError('selectedProduk', 'Produk sudah ada dalam daftar.');
+        if ($existingIndex !== false) {
+            $this->addError('selectedProduk', 'Produk sudah ada dalam daftar.');
         } else {
-            // Produk belum ada, tambahkan ke array selectedProduk
             $selectedProduct = Produk::find($productId);
 
             $this->selectedProduk[] = [
@@ -52,24 +54,78 @@ class Transaksi extends Component
 
     public function getTotalQuantity()
     {
-        $totalQuantity = 0;
-
-        foreach ($this->selectedProduk as $item) {
-            $totalQuantity += $item['quantity'];
-        }
-
-        return $totalQuantity;
+        return collect($this->selectedProduk)->sum('quantity');
     }
 
     public function getTotalPrice()
     {
-        $totalPrice = 0;
+        return collect($this->selectedProduk)->sum(function ($item) {
+            return $item['product']->harga * $item['quantity'];
+        });
+    }
 
-        foreach ($this->selectedProduk as $item) {
-            $totalPrice += $item['product']->harga * $item['quantity'];
+    public function calculateTotal()
+    {
+        $subtotal = collect($this->selectedProduk)->sum(function ($item) {
+            return $item['product']->harga * $item['quantity'];
+        });
+        $total = $subtotal + $this->feeAdmin * $this->qty;
+
+        return $total;
+    }
+
+
+    public function updated($propertyName)
+    {
+        $this->validateOnly($propertyName);
+    }
+
+    public function submitForm()
+    {
+        $this->validate();
+
+        try {
+            $total = $this->calculateTotal();
+            $this->saveToDatabase($total);
+            $this->resetForm();
+            session()->flash('success', 'Sukses menghitung produk');
+        } catch (\Exception $ex) {
+            $this->addError('error', 'Terjadi kesalahan: ' . $ex->getMessage());
+            session()->flash('error', 'Gagal menghitung produk');
         }
+    }
 
-        return $totalPrice;
+    private function saveToDatabase($total)
+    {
+        $pelangganId = $this->selectedPelangganId;
+
+        if ($pelangganId !== null) {
+            $penjualan = Penjualan::create([
+                'tanggal_penjualan' => now(),
+                'total_harga' => $total,
+                'pelanggan_id' => $pelangganId,
+            ]);
+
+            foreach ($this->selectedProduk as $item) {
+                DetailPenjualan::create([
+                    'penjualan_id' => $penjualan->id,
+                    'produk_id' => $item['product']->id,
+                    'total_produk' => $item['quantity'],
+                    'subtotal' => $item['product']->harga * $item['quantity'],
+                    'fee_admin' => $this->feeAdmin,
+                ]);
+            }
+            $this->dispatch('updateTotal', $total, $penjualan->id);
+        } else {
+            $this->addError('selectedPelangganId', 'Harap pilih pelanggan.');
+        }
+    }
+
+    private function resetForm()
+    {
+        $this->selectedProduk = [];
+        $this->feeAdmin = 0;
+        $this->selectedPelangganId = null;
     }
 
     public function render()
--- resources/views/livewire/transaksi.blade.php
+++ resources/views/livewire/transaksi.blade.php
@@ -19,18 +19,19 @@
                                 <li>
                                     <button x-on:click="isDropdownOpen = false; $wire.selectProduk({{ $item->id }})"
                                         type="button"
-                                        class="inline-flex w-full px-4 py-2 hover:bg-gray-100 dark:hover:bg-gray-600 dark:hover:text-white">{{ $item->name }}</button>
+                                        class="inline-flex w-full font-medium px-4 py-2 hover:bg-gray-100 dark:hover:bg-gray-600 dark:hover:text-white">
+                                        - {{ $item->name }}</button>
+                                    <hr>
                                 </li>
-                                <hr>
                             @endforeach
 
                         </ul>
                     </div>
                     <div class="relative w-full">
                         <input type="search" id="search-dropdown"
-                            class="block p-2.5 w-full z-20 text-sm text-gray-900 bg-gray-50 rounded-e-lg border-s-gray-50 border-s-2 border border-gray-300 focus:ring-orange-500 focus:border-orange-500 dark:bg-gray-700 dark:border-s-gray-700  dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:border-orange-500"
+                            class="block p-2.5 w-full z-20 text-sm transition text-gray-900 bg-gray-50 rounded-e-lg border-s-gray-50 border-s-2 border border-gray-300 focus:ring-orange-500 focus:border-orange-500 dark:bg-gray-700 dark:border-s-gray-700  dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:border-orange-500"
                             placeholder="Cari menggunakan kode produk..." required>
-                        <button type="submit"
+                        {{-- <button type="submit"
                             class="absolute top-0 end-0 p-2.5 text-sm font-medium h-full text-white bg-orange-700 rounded-e-lg border border-orange-700 hover:bg-orange-800 focus:ring-4 focus:outline-none focus:ring-orange-300 dark:bg-orange-600 dark:hover:bg-orange-700 dark:focus:ring-orange-800">
                             <svg class="w-4 h-4" aria-hidden="true" xmlns="http://www.w3.org/2000/svg" fill="none"
                                 viewBox="0 0 20 20">
@@ -38,7 +39,7 @@
                                     stroke-width="2" d="m19 19-4-4m0-7A7 7 0 1 1 1 8a7 7 0 0 1 14 0Z" />
                             </svg>
                             <span class="sr-only">Search</span>
-                        </button>
+                        </button> --}}
                     </div>
                 </div>
             </form>
@@ -46,17 +47,17 @@
         </div>
 
     </div>
-    <div class="relative overflow-x-auto shadow-md border">
+    <div class="relative overflow-x-auto shadow-md border rounded">
         <table class="w-full text-sm text-left rtl:text-right text-gray-500 dark:text-gray-400">
-            <thead class="text-xs text-gray-700 uppercase bg-gray-100 dark:bg-gray-700 dark:text-gray-400">
+            <thead class="text-xs text-gray-50 uppercase  bg-orange-500 dark:bg-gray-700 dark:text-gray-400">
                 <tr>
-                    <th scope="col" class="px-6 py-3 rounded-s-lg">
+                    <th scope="col" class="px-6 py-3">
                         Product name
                     </th>
                     <th scope="col" class="px-6 py-3">
                         Qty
                     </th>
-                    <th scope="col" class="px-6 py-3 rounded-e-lg">
+                    <th scope="col" class="px-6 py-3 ">
                         Harga
                     </th>
                 </tr>
@@ -82,16 +83,16 @@
                     </tr>
                 @empty
                     <tr>
-                        <td colspan="3" class="text-center text-2xl font-bold">No products selected</td>
+                        <td colspan="3" class="text-center text-2xl font-bold py-10">No products selected</td>
                     </tr>
                 @endforelse
             </tbody>
 
             <tfoot>
-                <tr class="font-semibold text-gray-900 dark:text-white">
-                    <th scope="row" class="px-6 py-3 text-2xl">Total : </th>
-                    <td class="px-6 py-3 text-xl">{{ $totalQuantity }}</td>
-                    <td class="px-6 py-3 text-xl">Rp.{{ number_format($totalPrice, 2) }}</td>
+                <tr class="font-semibold text-gray-900 dark:text-white border-t">
+                    <th scope="row" class="px-6 py-3 text-2xl text-gray-500">Total : </th>
+                    <td class="px-6 py-3 text-xl text-gray-500">{{ $totalQuantity }}</td>
+                    <td class="px-6 py-3 text-xl text-gray-500">Rp.{{ number_format($totalPrice, 2) }}</td>
                 </tr>
             </tfoot>
         </table>
@@ -100,8 +101,9 @@
         <div class="flex gap-10">
             <div class="grid gap-4 p-4 w-full max-w-xl">
                 <div>
-                    <select id="countries"
+                    <select wire:model="selectedPelangganId"
                         class="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded focus:ring-orange-500 focus:border-orange-500 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-orange-500 dark:focus:border-orange-500">
+                        <option selected>Pilih Pengguna</option>
                         @foreach ($pelanggan as $item)
                             <option value="{{ $item->id }}">{{ $item->nama }}</option>
                         @endforeach
@@ -113,6 +115,7 @@
                         placeholder="......." required>
                 </div>
             </div>
+
             <div class="border-l pl-7">
                 <div class="flex mt-4 gap-5">
                     <div>
@@ -125,15 +128,17 @@
                         <ul>
                             <li>
                                 <span class="font-medium">Rp.</span>
-                                <input type="number" name="" id="" class="h-8 mt-1" value="10">
+                                <input wire:model="feeAdmin" type="number" name="" id=""
+                                    class="h-8 mt-1">
                             </li>
                             <li class="mt-7 font-medium text-xl">
-                                Rp.0.00
+                                Rp.{{ number_format($this->calculateTotal(), 2) }}
                             </li>
                         </ul>
                     </div>
                 </div>
             </div>
+
         </div>
         <div class="bg-gray-50 h-20 border-t">
             <div class="flex justify-between p-4">
@@ -149,12 +154,65 @@
 
                 </div>
                 <div class="flex gap-5">
-                    <button
-                        class="bg-orange-400 text-white px-8 py-2 rounded font-medium hover:bg-orange-500 transition duration-300 shadow-md">Submit</button>
+                    <button wire:click="submitForm"
+                        class="bg-orange-400 text-white px-8 py-2 rounded font-medium hover:bg-orange-500 transition duration-300 shadow-md">
+                        Submit
+                    </button>
                     <button
                         class="bg-gray-400 text-white px-8 py-2 rounded font-medium hover:bg-gray-500 transition duration-300 shadow-md">Batal</button>
                 </div>
             </div>
         </div>
     </div>
+
+    @if (session()->has('success'))
+        <div id="alert-sukses"
+            class="flex bottom-0 w-full max-w-md right-2 shadow-lg rounded-xl absolute items-center p-4 mb-4 text-lime-800 border-t-4 border-lime-300 bg-lime-50 dark:text-lime-400 dark:bg-gray-800 dark:border-lime-800"
+            role="alert">
+            <svg class="flex-shrink-0 w-4 h-4" xmlns="http://www.w3.org/2000/svg" id="Layer_1" data-name="Layer 1"
+                viewBox="0 0 24 24" width="512" height="512">
+                <path
+                    d="m18.214,9.098c.387.394.381,1.027-.014,1.414l-4.426,4.345c-.783.768-1.791,1.151-2.8,1.151-.998,0-1.996-.376-2.776-1.129l-1.899-1.867c-.394-.387-.399-1.02-.012-1.414.386-.395,1.021-.4,1.414-.012l1.893,1.861c.776.75,2.001.746,2.781-.018l4.425-4.344c.393-.388,1.024-.381,1.414.013Zm5.786,2.902c0,6.617-5.383,12-12,12S0,18.617,0,12,5.383,0,12,0s12,5.383,12,12Zm-2,0c0-5.514-4.486-10-10-10S2,6.486,2,12s4.486,10,10,10,10-4.486,10-10Z" />
+            </svg>
+            <div class="ms-3 text-sm font-medium">
+                {{ session('success') }}
+            </div>
+            <button type="button"
+                class="ms-auto -mx-1.5 -my-1.5 bg-lime-50 text-lime-500 rounded-lg focus:ring-2 focus:ring-lime-400 p-1.5 hover:bg-lime-200 inline-flex items-center justify-center h-8 w-8 dark:bg-gray-800 dark:text-lime-400 dark:hover:bg-gray-700"
+                data-dismiss-target="#alert-sukses" aria-label="Close">
+                <span class="sr-only">Dismiss</span>
+                <svg class="w-3 h-3" aria-hidden="true" xmlns="http://www.w3.org/2000/svg" fill="none"
+                    viewBox="0 0 14 14">
+                    <path stroke="currentColor" stroke-linecap="round" stroke-linejoin="round" stroke-width="2"
+                        d="m1 1 6 6m0 0 6 6M7 7l6-6M7 7l-6 6" />
+                </svg>
+            </button>
+        </div>
+    @endif
+
+    @if (session()->has('error'))
+        <div id="alert-error"
+            class="flex bottom-0 w-full max-w-md right-2 shadow-lg rounded-xl absolute items-center p-4 mb-4 text-red-800 border-t-4 border-red-300 bg-red-50 dark:text-red-400 dark:bg-gray-800 dark:border-red-800"
+            role="alert">
+
+            <svg class="flex-shrink-0 w-4 h-4" xmlns="http://www.w3.org/2000/svg" id="Layer_1" data-name="Layer 1"
+                viewBox="0 0 24 24" width="512" height="512">
+                <path
+                    d="m15.707,9.707l-2.293,2.293,2.293,2.293c.391.391.391,1.023,0,1.414-.195.195-.451.293-.707.293s-.512-.098-.707-.293l-2.293-2.293-2.293,2.293c-.195.195-.451.293-.707.293s-.512-.098-.707-.293c-.391-.391-.391-1.023,0-1.414l2.293-2.293-2.293-2.293c-.391-.391-.391-1.023,0-1.414s1.023-.391,1.414,0l2.293,2.293,2.293-2.293c.391-.391,1.023-.391,1.414,0s.391,1.023,0,1.414Zm8.293,2.293c0,6.617-5.383,12-12,12S0,18.617,0,12,5.383,0,12,0s12,5.383,12,12Zm-2,0c0-5.514-4.486-10-10-10S2,6.486,2,12s4.486,10,10,10,10-4.486,10-10Z" />
+            </svg>
+            <div class="ms-3 text-sm font-medium">
+                {{ session('error') }}
+            </div>
+            <button type="button"
+                class="ms-auto -mx-1.5 -my-1.5 bg-red-50 text-red-500 rounded-lg focus:ring-2 focus:ring-red-400 p-1.5 hover:bg-red-200 inline-flex items-center justify-center h-8 w-8 dark:bg-gray-800 dark:text-red-400 dark:hover:bg-gray-700"
+                data-dismiss-target="#alert-error" aria-label="Close">
+                <span class="sr-only">Dismiss</span>
+                <svg class="w-3 h-3" aria-hidden="true" xmlns="http://www.w3.org/2000/svg" fill="none"
+                    viewBox="0 0 14 14">
+                    <path stroke="currentColor" stroke-linecap="round" stroke-linejoin="round" stroke-width="2"
+                        d="m1 1 6 6m0 0 6 6M7 7l6-6M7 7l-6 6" />
+                </svg>
+            </button>
+        </div>
+    @endif
 </div>
